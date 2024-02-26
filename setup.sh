94b0b5d1eb71d62bd905f6d9adaed0e0919d7dc3
Coenraad Human <coen.human@gmail.com>
2024-02-26 15:13:59 +0200

    feat: initial script and files

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/linux-terminal-setup/nerd-fonts-setup/main/setup.sh | bash
+
+sudo apt install snapd curl -y
+
+sudo snap install alacritty --classic
+
+mkdir -p ~/.config/alacritty/
+
+curl  -o ~/.config/alacritty/alacritty.toml
+
+
